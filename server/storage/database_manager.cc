a3d322947f16b07b99209a5558075688141bd04b
Noxy3301 <curelia@outlook.jp>
2025-08-19 03:05:44 +0900

    change: switch LineairDB concurrency control from SiloNWR to Silo

--- server/storage/database_manager.cc
+++ server/storage/database_manager.cc
@@ -9,6 +9,7 @@ DatabaseManager::DatabaseManager() {
     conf.enable_checkpointing = false;
     conf.enable_recovery      = false;
     conf.max_thread           = 1;  // TODO: multi threading?
+    conf.concurrency_control_protocol = LineairDB::Config::Silo;
     database_ = std::make_shared<LineairDB::Database>(conf);
     std::cout << "Database manager initialized" << std::endl;
 }

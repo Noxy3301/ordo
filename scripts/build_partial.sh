19e23e7e904e6250838824c4dc21446038dd4436
Noxy3301 <curelia@outlook.jp>
2025-08-19 03:08:02 +0900

    add: build_partial.sh

---
+++ scripts/build_partial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+echo "Building server..."
+cd build/server
+make -j `nproc`
+cd ../..
+
+echo "Building proxy..."
+cp -r proxy build/
+cd build
+ninja ha_lineairdb_storage_engine.so

b262deff61dfe7776b3371a34490215e8152390f
Noxy3301 <curelia@outlook.jp>
2025-08-21 04:28:40 +0900

    add: start_mysql.sh

---
+++ scripts/start_mysql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Initialize and Start MySQL Server
+cd build
+./runtime_output_directory/mysqld --initialize-insecure --user=$USER --datadir=./data
+./runtime_output_directory/mysqld --datadir=./data --socket=/tmp/mysql.sock --port=3307 &

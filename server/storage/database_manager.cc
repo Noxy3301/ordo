1d2da1b71597bf91c8c0e8c777fae3c417d627d8
Noxy3301 <curelia@outlook.jp>
2025-08-26 15:20:05 +0900

    fix: correct Silo concurrency control enum type

--- server/storage/database_manager.cc
+++ server/storage/database_manager.cc
@@ -10,7 +10,7 @@ DatabaseManager::DatabaseManager() {
     conf.enable_checkpointing = false;
     conf.enable_recovery      = false;
     conf.max_thread           = 1;  // TODO: multi threading?
-    conf.concurrency_control_protocol = LineairDB::Config::Silo;
+    conf.concurrency_control_protocol = LineairDB::Config::ConcurrencyControl::Silo;
     database_ = std::make_shared<LineairDB::Database>(conf);
     LOG_INFO("Database manager initialized");
 }

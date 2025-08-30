5c07bc09e154247d7ea4629b1ffe4b1b48bf846b
Noxy3301 <curelia@outlook.jp>
2025-08-31 01:54:08 +0900

    chore: increase max_thread from 1 to 16

--- server/storage/database_manager.cc
+++ server/storage/database_manager.cc
@@ -9,7 +9,7 @@ DatabaseManager::DatabaseManager() {
     LineairDB::Config conf;
     conf.enable_checkpointing = false;
     conf.enable_recovery      = false;
-    conf.max_thread           = 1;  // TODO: multi threading?
+    conf.max_thread           = 16;  // TODO: multi threading?
     conf.concurrency_control_protocol = LineairDB::Config::ConcurrencyControl::Silo;
     database_ = std::make_shared<LineairDB::Database>(conf);
     LOG_INFO("Database manager initialized");
